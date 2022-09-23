60315150bd087d12f46ad67af8526429736c377f
Marco Oquendo <marcooquendoc@gmail.com>
2022-09-23 14:26:56 -0500

    Set hooks and methods onChange and submit

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,13 +1,55 @@
-import React from 'react';
+/* eslint-disable no-multiple-empty-lines */
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const InputBook = () => (
-  <form action="" className="form-add-book">
-    <input type="text" name="bookTitle" id="book-title" placeholder="title" required />
-    <select name="category" id="category">
-      <option value="" disabled selected>Category</option>
-    </select>
-    <input type="submit" value="ADD BOOK" />
-  </form>
-);
+const InputBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const submitBook = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+      category,
+    };
+    e.target.reset();
+
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <form onSubmit={submitBook} className="form-add-book">
+      <input onChange={onTitleChange} type="text" name="bookTitle" id="book-title" placeholder="title" required value={title} />
+      <input onChange={onAuthorChange} type="text" name="bookAuthor" id="book-author" placeholder="author" required value={author} />
+      <input onChange={onCategoryChange} type="text" name="category" id="book-category" placeholder="category" required value={category} />
+
+      <input type="submit" value="ADD BOOK" />
+    </form>
+  );
+};
 
 export default InputBook;
+
+
+
+
+
+
